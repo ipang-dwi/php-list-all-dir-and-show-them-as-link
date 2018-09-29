db05c5f160dcaa74beb374bb9cc928a5f3353436
ipang dwi <admin@firstplato.com>
2018-09-29 14:56:07 +0700

    create dir.php
    create dir.php

---
+++ dir.php
@@ -0,0 +1,10 @@
+<?php
+    $dir    = '[YOUR-DIR-NAME]';
+    $files = scandir($dir);
+    
+    $no = 1;
+    foreach($files as $row){
+        if(is_dir($row)==1 and $row != '.' and $row != '..' and $row != '.well-known' and $row != 'cgi-bin' and $row != 'css' and $row != 'js') 
+            echo $no++.". <a href='https://www.example.com/".$row."' target='_blank'>".$row."</a><br>";
+    }
+?>
